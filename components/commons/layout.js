0c999691d6e491016aabb784addc226e4faf468a
Danish_Ahmed <qureshidanish78@gmail.com>
2021-07-16 13:30:19 +0500

    Site title update

--- components/commons/layout.js
+++ components/commons/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "[Banagel]";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Bangel Beta";
 
 export default function Layout({ children, home }) {
   return (
