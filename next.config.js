a20f3fde4a58aca2d3d8d405a73257a002713a50
Danish_Ahmed <qureshidanish78@gmail.com>
2021-07-25 14:25:54 +0500

    Sunday Commit 3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ["http://banagel.herokuapp.com"],
+    domains: ["https://banagel.herokuapp.com"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
 };
